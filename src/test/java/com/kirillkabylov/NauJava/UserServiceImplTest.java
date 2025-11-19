c65cf5f2a34a39141043b7d89f092d22de95fdde
lollicry <2503kirill@gmail.com>
2025-11-19 21:01:07 +0400

    Заданиe 3

--- src/test/java/com/kirillkabylov/NauJava/UserServiceImplTest.java
+++ src/test/java/com/kirillkabylov/NauJava/UserServiceImplTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
